7cf8f7f9206f6937b5bad9f68fac35b2c0c81b3f
Rithvik Reddygari <reddygari.rithvik@gmail.com>
2026-01-08 15:26:58 -0500

    added tests, fixed issues, formatted

--- src/sparseappbench/benchmarks/transitive_closure.py
+++ src/sparseappbench/benchmarks/transitive_closure.py
@@ -3,14 +3,14 @@ Name: Transitive Closure + Simply Connected Components
 Author: Rithvik Reddygari
 Email: rreddygari3@gatech.edu
 Motivation (Importance of problem with citation):
-“Connected component labeling is a key step in a wide-range of applications, 
-such as community detection in social networks and coherent region 
+“Connected component labeling is a key step in a wide-range of applications,
+such as community detection in social networks and coherent region
 tracking in image analysis.”
 J. Iverson, C. Kamath, and G. Karypis, “Evaluation of connected-component labeling algorithms
  for distributed-memory systems,” Parallel Computing, vol. 44, pp. 53–68, May 2015, doi: 10.1016/j.parco.2015.02.005.
 
-“Reachability queries, which ask whether there exists a path between two vertices in a directed graph, 
-are fundamental operations in graph databases and are widely used in applications such 
+“Reachability queries, which ask whether there exists a path between two vertices in a directed graph,
+are fundamental operations in graph databases and are widely used in applications such
 as XML querying, program analysis, social networks, and biological networks.
 Computing the transitive closure is a classical approach to answering reachability queries.”
 
@@ -32,16 +32,16 @@ Author: Joel Mathew Cherian
 Email: jcherian32@gatech.edu
 Reference: Kepner, Jeremy, and John Gilbert, eds. Graph algorithms in the language of linear algebra
 
-Data Generation (How is the data generated? Why is it realistic?):
-
 Generative AI: No generative AI was used to construct the benchmark function
 itself. Generative AI might have been used to construct tests. This statement
 was written by hand.
 """
 
+
 def benchmark_transitive_closure(xp, edges_b):
     graph = _transitive_closure_computation(xp, edges_b)
     return xp.to_benchmark(graph)
+
 
 def benchmark_simple_connected_components(xp, edges_b):
     graph = _transitive_closure_computation(xp, edges_b)
@@ -49,6 +49,7 @@ def benchmark_simple_connected_components(xp, edges_b):
     res = xp.einsum("res[i,j] = graph[i,j] & graph[j,i]", graph=graph)
     res = xp.compute(res)
     return xp.to_benchmark(res)
+
 
 def _transitive_closure_computation(xp, edges_b):
     edges = xp.from_benchmark(edges_b)
@@ -65,7 +66,9 @@ def _transitive_closure_computation(xp, edges_b):
     max_iterations = n
     for iteration in range(max_iterations):
         graph = xp.lazy(graph)
-        nextGraph = xp.compute(xp.einsum("nextGraph[i,j] |= graph[i,k] & graph[k,j]", graph=graph))
+        nextGraph = xp.compute(
+            xp.einsum("nextGraph[i,j] or= graph[i,k] & graph[k,j]", graph=graph)
+        )
         if xp.compute(xp.all(xp.equal(xp.compute(graph), nextGraph))):
             break
         graph = nextGraph
